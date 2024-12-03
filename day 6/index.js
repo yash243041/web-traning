4a0098667f7f3fbfe2297c739f963d5a12c51d8b
yash243041 <yash_243041@saitm.ac.in>
2024-12-03 12:51:09 +0530

    Add files via upload

---
+++ day 6/index.js
@@ -0,0 +1,31 @@
+let a = 5;
+const b = 5;
+var c = a + b;
+console.log("hello worlds:",c);
+//variables : let,const,var-------,
+document.write("hello world");
+let e = 5;
+console.log(e);
+var d=5;
+console.log(d);
+const n=10;
+console.log(n);
+ //loops: for and while;
+ let count = 1;
+ let i = 9;
+     while(count<=10) {
+        let table = (i*count);
+        console.log("9*",count,"=",table);
+        count++;
+     }
+     const userinput = 10;
+     for(let i = 1; i<=10;i++){
+        const pahade = userinput + "*"+i+"="+userinput*i;
+        console.log(pahade);
+     }
+    for(let i=1; i<=10;i++){
+        const pahade = useinput + "*"+i+"="+userinput*i;
+        console.log(pahade);
+
+    }
+    
