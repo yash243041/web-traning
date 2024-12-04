fd01b3f1b2375a0003df57e37c9a19606afb8a97
yash243041 <yash_243041@saitm.ac.in>
2024-12-04 13:03:28 +0530

    Add files via upload

---
+++ day.js
@@ -0,0 +1,34 @@
+//Select:  
+const title = document.getElementById("h1");
+//new constant variable;
+
+const newContent ="----------12345---------";
+//assign value ofIng;
+title.innertext = newContent;
+const divEle = document.getElementsByClassName("title");
+//        0  1  2  3 ,.......
+//divEle = [div,div,div,div,----];
+
+divEle[2].innerText = "Third Div Tag";
+//console.log(divEle);
+for(let i = 0; 1<DocumentTimeline.length;i++) {
+    if(1%2 ===0){
+    // Even index:
+    divEle[1].innerText = "even";
+    } else {
+        divEle[1].innerText = "odd";
+
+    }
+}
+//Array;
+
+//index : 0  1  2  3  4  5  6  7  8  9  
+const arr = [10, 9 ,8 ,7 ,6 ,5 ,4 ,3 ,2 ,1];
+//print specific element of an array:
+//syntax : array_name[index]
+//length of an array : array_name length 
+const FirstElement = arr[0];
+for(let i=0;i<arr.length)
+console.log(FirstElement);
+
+console.log(secondElement);
